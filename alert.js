fd2242d6de47a744bd6e6793ca59ef7128f1c5fb
Olexiy Koval <kalex32@ukr.net>
2018-11-18 01:20:02 +0200

    Proceed JS

--- alert.js
+++ alert.js
@@ -1,1 +1,16 @@
 alert('Привет Мир!')
+
+/*
+var admin, name;
+name = "Василий";
+admin = name;
+alert(admin);*/
+
+/*
+var nameOurPlanet = "Земля";
+
+var nameClient = "Петя";
+*/
+
+var yourName = prompt("Введите Ваше имя", 'Алексей');
+confirm("Вас зовут "+yourName+"?");
