292bdde6333b911a28aa83fcd25b4d9c96225b5b
Olexiy Koval <kalex32@ukr.net>
2018-11-19 00:19:15 +0200

    if_else

--- alert.js
+++ alert.js
@@ -1,4 +1,4 @@
-alert('Привет Мир!')
+alert('Привет Мир!');
 
 /*
 var admin, name;
@@ -12,5 +12,50 @@ var nameOurPlanet = "Земля";
 var nameClient = "Петя";
 */
 
+/*
 var yourName = prompt("Введите Ваше имя", 'Алексей');
-confirm("Вас зовут "+yourName+"?");
+confirm("Вас зовут "+yourName+"?");*/
+
+/*if ("0"){
+    alert('Привет!')
+}*/
+/*
+var name = prompt("Каково «официальное» название JavaScript?", '');
+if (name == "ECMAScript"){
+    alert("Верно!");
+} else {
+    alert("Не знаете? «ECMAScript»!")
+}*/
+
+/*
+var value = prompt("Введите число", '');
+if (value>0){
+    alert(1);
+} else if (value<0){
+    alert(-1);
+} else alert(0);*/
+
+/*var entrance = prompt("Логин:", '');
+if (entrance == "Админ") {
+    var password = prompt("Пароль", '');
+    if (password == "Чёрный Властелин") {
+        alert("Добро пожаловать!");
+    } else if (password == null) {
+        alert("Вход отменён");
+    } else {
+        alert("Пароль неверный");
+    }
+} else if (entrance == null) {
+    alert("Вход отменён");
+} else {
+    alert("Я Вас не знаю");
+}*/
+
+/*
+var login;
+var message;
+
+login == 'Вася'? message = 'Привет':
+login == 'Директор'? message = 'Здравствуйте':
+login == ''? message = 'Нет логина': message = ''
+*/
